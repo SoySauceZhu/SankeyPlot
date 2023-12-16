2e1ec451faac1779a8252acc45fc5e6db8b6f73c
Mingjie.Zhu22 <zhumingjie0627@outlook.com>
2023-12-17 00:58:03 +0800

    Complete Launcher, Plot, Pane

--- src/SankeyPane.java
+++ src/SankeyPane.java
@@ -2,21 +2,20 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
-import java.util.Map;
-
-public class SankeyPane extends Pane {
+public class myPane extends Pane {
 
     String filename;
     SankeyReader reader;
 
     double xLeftMost = 75;
+    double ZOOM = 0.75;
     Font defaultFont = new Font("Arial", 18);
     Font labelFont = new Font("Arial", 22);
 
 
 
     // Constructor: takes in the filename
-    public SankeyPane(String fn) {
+    public myPane(String fn) {
         filename = fn;
         reader = new SankeyReader(fn);
     }
@@ -29,16 +28,16 @@ public class SankeyPane extends Pane {
         SankeyText text0 = new SankeyText(reader.label + "\n[" + reader.returnSum() + "]",
                 xLeftMost, getHeight() / 2, labelFont);
         SankeyNode node0 = new SankeyNode(xLeftMost + text0.width,
-                (getHeight() - reader.returnSum()) / 2, reader.returnSum(),
+                (getHeight() - reader.returnSum() * ZOOM) / 2, ZOOM * reader.returnSum(),
                 Color.GREEN.darker());
 
         getChildren().clear();
         getChildren().addAll(text0, node0);
 
 
-        double deltaY = (getHeight() - reader.returnSum()) / (reader.data.size() + 1);
-        double lowerNodeY = 0;
-        double lowerLinkY = node0.xPos;
+        double deltaY = (getHeight() - reader.returnSum() * ZOOM) / (reader.data.size() + 1) * 0.75;
+        double lowerNodeY = 70;
+        double lowerLinkY = node0.yPos;
 
         for (String str: reader.data.keySet()) {
 
@@ -46,13 +45,12 @@ public class SankeyPane extends Pane {
             Color color = SankeyColor.getColor();
             String string = str + "\n[" + size + "]";
 
-            SankeyNode node = new SankeyNode(0.8 * getWidth(), lowerNodeY + deltaY, size, color);
+            SankeyNode node = new SankeyNode(0.8 * getWidth(), lowerNodeY + deltaY, size * ZOOM, color);
             SankeyLinker linker = new SankeyLinker(xLeftMost + text0.width, lowerLinkY, node, color);
             SankeyText text = new SankeyText(string, 0.8 * getWidth() + 20, lowerNodeY + deltaY + size /2 + 5, defaultFont);
 
             getChildren().addAll(node, text, linker);
 
-            getChildren().clear();
 
             lowerLinkY = linker.lowerY;
             lowerNodeY = node.lowerY;
--- src/SankeyPlot.java
+++ src/SankeyPlot.java
@@ -1,101 +1,37 @@
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
-
-import java.util.Map;
 
 
-/* This myPlot class takes in the fileName from constructor and show the SankeyPlot */
-public class SankeyPlot extends Application {
-    String fileName;
-    Map<String, Double> dataDic;
-    String title;
-    String label;
 
-    double deltaX = 500;    // The distance from left node to right nodes
-    double deltaY = 50;
-    double ZOOM = 0.50;
-    Font defaultFont = new Font("Arial", 18);
-    Font labelFont = new Font("Arial", 22);
+/* myPlot class under construction:
+    1. good init position arrangement
+    2. resizing
+    3. aesthetic
+    4. color random
+ */
 
-    double xInit = 75; // The x-coordinate for the left most part of whole graph
-    double yInit;
-    double endX = deltaX + xInit;   // The x for end nodes column
-    double endY = 25;
+public class myPlot extends Application {
 
-    public SankeyPlot(String fn) {
-        this.fileName = fn;
+    String fileName = "D:\\Zhu22\\SynologyDrive\\1.2022_Year2\\4.JAVA\\CourseWork3\\data.txt";
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        SankeyReader reader = new SankeyReader(fileName);
+        Pane root = new myPane(fileName);
+        Scene scene = new Scene(root, 1000, 750);
+        primaryStage.setTitle(reader.title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
-
-    public SankeyPlot() {
-        // Test Constructor
-        this.fileName = "D:\\Zhu22\\SynologyDrive\\1.2022_Year2\\4.JAVA\\CourseWork3\\data.txt";
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage stage) {
-
-        stage.setWidth(1000);
-        stage.setHeight(500);
-        stage.setTitle(title);
-        Scene scene = new Scene(new Group());
-        Pane root = new Pane();
-
-        this.yInit = 200;
-
-        // Read in the file data
-        SankeyReader reader = new SankeyReader(fileName);
-        this.dataDic = reader.data;
-        this.title = reader.title;
-        this.label = reader.label;
-
-
-//        this.yInit = stage.getHeight()/2 - reader.returnSum()/2;
-
-        // Iterate to add Nodes and Linkers to root
-        SankeyText text0 = new SankeyText(label + "\n[" + reader.returnSum() + "]", xInit - 20, yInit + reader.returnSum() / 2 * ZOOM, labelFont);
-        SankeyNode node0 = new SankeyNode(xInit + text0.width, yInit, reader.returnSum() * ZOOM, Color.GREEN.darker());
-        root.getChildren().add(text0);
-        root.getChildren().add(node0);
-
-        double lowerNodeY = endY;
-        double lowerLinkY = yInit;
-        for (String str : dataDic.keySet()) {
-            // Draw a link and a node
-
-            double size = dataDic.get(str) ;
-            Color color = SankeyColor.getColor();
-            str = str + "\n[" + size + "]";
-
-            SankeyNode node = new SankeyNode(endX, lowerNodeY + deltaY, size * ZOOM, color);
-            SankeyLinker linker = new SankeyLinker(xInit + text0.width, lowerLinkY, node, color);
-            SankeyText text = new SankeyText(str, endX + 20, lowerNodeY + deltaY  + size * ZOOM / 2 + 5, defaultFont);
-
-
-//            SankeyLinker linker = new SankeyLinker(new SankeyNode(xInit + text0.width, lowerLinkY, size, color), node, color);
-
-
-            root.getChildren().addAll(node, text, linker);
-
-            lowerLinkY = linker.lowerY;
-            lowerNodeY = node.lowerY;
-        }
-
-
-        // Show everything
-        scene.setRoot(root);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-
 }
+
