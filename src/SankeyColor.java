7feb06a1f8b3b04ff71ca915f08e92de43d7e192
Soy_SauceZhu <zhumingjie0627@outlook.com>
2023-12-17 17:35:12 +0800

    Update SankeyColor.java

--- src/SankeyColor.java
+++ src/SankeyColor.java
@@ -2,7 +2,6 @@ import javafx.scene.paint.Color;
 
 public class SankeyColor {
     static int count = 0;
-    //    static Color[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW.darker(), Color.PURPLE};
     static Color[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW.darker(), Color.PURPLE};
 
     public static Color getColor() {
